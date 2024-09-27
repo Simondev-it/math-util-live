778d6090898ec4036c223475118fcf9f4e687f32
Simondev-it <nguyenthanhduong37923@gmail.com>
2024-09-28 01:39:30 +0700

    Lan dau lam chuyen ay-dua code len Server

---
+++ math-util-live/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace math_util_live
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("this code test for git ");
+        }
+    }
+}
