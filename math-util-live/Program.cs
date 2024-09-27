93aa811f2e037442b6993c201de9f385b047b2c6
Simondev-it <nguyenthanhduong37923@gmail.com>
2024-09-28 01:59:14 +0700

    Lan thu 2 lam chuyen ay

--- math-util-live/Program.cs
+++ math-util-live/Program.cs
@@ -7,6 +7,21 @@
             Console.WriteLine("Hello, World!");
 
             Console.WriteLine("this code test for git ");
+
+            SumAll();
+
+            
+        }
+
+        static void SumAll()
+        {
+            int sum = 0;
+            for (int i = 1; i <= 10; i++)
+
+                sum += i;
+            Console.WriteLine("Tổng giá trị từ 1 đến 10 là :{0} ",sum );
+
+
         }
     }
 }
